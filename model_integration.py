3a051550d07de5a27f6874d6efb0c8b4ade1ec0b
Karandeep Singh <53494842+krndxs@users.noreply.github.com>
2021-08-18 13:56:18 -0700

    Update model_integration.py

--- model_integration.py
+++ model_integration.py
@@ -30,7 +30,7 @@ class HospitalPricingClassifier(BaseEstimator, ClassifierMixin):
         self.hospital_loc = pd.read_parquet(HospitalLocPath)
         pb = pickle.PickleBuffer(b"pickle_prices.pkl")
         data = pickle.dumps(pb, protocol=5)
-        assert pickle.loads(data) == b"pickle_prices.pkl"
+        self.prices = (pickle.loads(data) == b"pickle_prices.pkl")
             
 
     def _get_distance(self,p_lat, p_lng, threshold=100):
