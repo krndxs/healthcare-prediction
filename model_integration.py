3f02599528e36e7f6c03b1717d3c8157fc31d54d
Karandeep Singh <53494842+krndxs@users.noreply.github.com>
2021-08-18 15:08:50 -0700

    Update model_integration.py

--- model_integration.py
+++ model_integration.py
@@ -32,7 +32,7 @@ class HospitalPricingClassifier(BaseEstimator, ClassifierMixin):
         data = pickle.dumps(pb, protocol=5)
         file = pickle.loads(data)
         self.prices = pd.read_pickle(file)
-        print("FILE PRINOUT HERE", file)
+        print("FILE PRINOUT HERE, CHECK OUTPUT", file)
 
             
 
